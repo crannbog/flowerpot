940a4327fb58b507570c6906fd1c63b555150b23
@cmmdmx <me@cmmdmx.de>
2024-10-20 12:37:14 +0200

    remove disk speed test

--- core/helper/logSysInfo.lua
+++ core/helper/logSysInfo.lua
@@ -12,73 +12,9 @@ package.path = package.path .. ";../../?.lua"
 local logger = require("core.helper.logger")
 local exec = require("core.helper.exec")
 
-local function convert_speed(speed)
-    local value, unit = speed:match("([0-9.]+)%s*(%a+)")
-    value = tonumber(value)
-    if unit == "MB/s" and value >= 1024 then
-        value = value / 1024
-        unit = "GB/s"
-    elseif unit == "GB/s" and value < 1 then
-        value = value * 1024
-        unit = "MB/s"
-    end
-    return value, unit
-end
-
-local function speed_test(filename)
-    local fileSize = "8192"
-
-    -- Write speed test
-    local write_speed = exec.silent("dd if=/dev/zero of=" .. filename .. " bs=64K count=" .. fileSize .. " conv=fdatasync,notrunc 2>&1 | grep -o '[0-9.]* [MG]B/s'", true)
-    local write_value, write_unit = convert_speed(write_speed)
-
-    -- Read speed test
-    local read_speed = exec.silent("dd if=" .. filename .. " of=/dev/null bs=64K count=" .. fileSize .. " 2>&1 | grep -o '[0-9.]* [MG]B/s'", true)
-    local read_value, read_unit = convert_speed(read_speed)
-
-    return write_value, write_unit, read_value, read_unit
-end
-
 local function measure_network_speed()
     logger.plain(logger.add_whitespaces("Testing Network Speed..."))
     exec.run("speedtest-cli --simple", true)
-end
-
-local function measure_disk_speed()
-    logger.plain(logger.add_whitespaces("Testing Disk Speed..."))
-
-    local total_write_speed = 0
-    local total_read_speed = 0
-    local num_tests = 10
-    local w_unit = "?"
-    local r_unit = "?"
-
-    local testfile = "ff.speedtestfile"
-
-    for i = 1, num_tests do
-        local write_value, write_unit, read_value, read_unit = speed_test(testfile)
-        total_write_speed = total_write_speed + write_value
-        total_read_speed = total_read_speed + read_value
-        w_unit = write_unit
-        r_unit = read_unit
-    end
-
-    local avg_write_speed = total_write_speed / num_tests
-    local avg_read_speed = total_read_speed / num_tests
-
-    logger.plain(
-        logger.add_whitespaces(
-            string.format("Average Write Speed: %.2f %s/s", avg_write_speed, w_unit)
-        )
-    )
-    logger.plain(
-        logger.add_whitespaces(
-            string.format("Average Read Speed: %.2f %s/s", avg_read_speed, r_unit)
-        )
-    )
-    logger.plain("")
-
-    os.remove(testfile)
 end
 
 local function logSysInfo()
@@ -117,8 +53,6 @@ local function logSysInfo()
             fi
         done
     ]], true)
-
-    measure_disk_speed()
 
     exec.run([[
         echo "# PUBLIC IP:" & ip -4 addr show dev $(ip route show default | awk '/default/ {print $5}') | grep inet | awk '{print $2}' | awk -F'/' '{print $1}'
